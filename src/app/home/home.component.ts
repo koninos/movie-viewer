db4de9e464e3e9f527ed76bd0a03b5aee63df4d9
Konstantinos Gialouris <koninosgial@hotmail.com>
2020-03-28 20:27:35 +0200

    Enhance Home page & styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { faHeartbeat } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  public pageTitle: string = 'Welcome to Movie Viewer';
+  public faHeartbeat = faHeartbeat;
 
 }
